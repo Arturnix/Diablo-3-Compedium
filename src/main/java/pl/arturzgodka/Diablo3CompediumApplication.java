3857c070527df9d8277b77074a4fbef0a338615d
Arturnix <artur.zgodka.programista@gmail.com>
2024-04-22 18:49:26 +0200

    Add project files

---
+++ src/main/java/pl/arturzgodka/Diablo3CompediumApplication.java
@@ -0,0 +1,13 @@
+package pl.arturzgodka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diablo3CompediumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Diablo3CompediumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/arturzgodka/Diablo3CompediumApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.arturzgodka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Diablo3CompediumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
