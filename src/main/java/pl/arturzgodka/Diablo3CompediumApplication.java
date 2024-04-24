79efac09aba07bce4ff731e12b15818dbf7b244b
Arturnix <artur.zgodka.programista@gmail.com>
2024-04-24 21:22:05 +0200

    Update configuration files

--- src/main/java/pl/arturzgodka/Diablo3CompediumApplication.java
+++ src/main/java/pl/arturzgodka/Diablo3CompediumApplication.java
@@ -2,12 +2,20 @@ package pl.arturzgodka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.arturzgodka.databaseutils.UserDao;
+import pl.arturzgodka.datamodel.User;
 
 @SpringBootApplication
 public class Diablo3CompediumApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Diablo3CompediumApplication.class, args);
+
+        User user1 = new User();
+        UserDao userDao = new UserDao();
+
+        userDao.saveUser(user1);
+
     }
 
 }
