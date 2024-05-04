ec7b8dd8b41b5ec28e56ef0c5a4c7885da5a98c3
Arturnix <artur.zgodka.programista@gmail.com>
2024-05-04 09:53:46 +0200

    Add client credentials

---
+++ src/main/java/pl/arturzgodka/ClientCredentials.java
@@ -0,0 +1,15 @@
+package pl.arturzgodka;
+
+public class ClientCredentials {
+
+    private static final String clientId = "1754818f260847d3822b294f3f7d692d";
+    private static final String clientSecret = "ilMznRmswcJ4fLMDBweYNBq9RC3URJLL";
+
+    public static String getClientId() {
+        return clientId;
+    }
+
+    public static String getClientSecret() {
+        return clientSecret;
+    }
+}
