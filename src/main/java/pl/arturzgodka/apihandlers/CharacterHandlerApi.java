9f1d6bf655821cd9e0bdc26a5e3210770984ff12
Arturnix <artur.zgodka.programista@gmail.com>
2024-05-04 13:02:39 +0200

    Rename character handler api class

--- src/main/java/pl/arturzgodka/apihandlers/CharacterHandlerApi.java
+++ src/main/java/pl/arturzgodka/apihandlers/CharacterHandlerApi.java
@@ -3,7 +3,7 @@ package pl.arturzgodka.apihandlers;
 import pl.arturzgodka.token.FetchToken;
 import pl.arturzgodka.token.Token;
 
-public class HeroHandlerApi {
+public class CharacterHandlerApi {
     //Hero ID: 170761702
     //zrobic jako static?
     public static String generateRequest(String battleTag, String heroId, FetchToken fetchToken) { //token przypisywać ze zmiennej. Pobieram token kiedy program startuje i wrzucam go do stałej.
