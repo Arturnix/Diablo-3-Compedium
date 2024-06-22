2d678a5fa5b77a496d3afb30b4410c07315063f0
Arturnix <artur.zgodka.programista@gmail.com>
2024-06-22 17:42:26 +0200

    Reduce number of class skills pages to one skills page, with skills list passed through model attribute

---
+++ src/main/java/pl/arturzgodka/controllers/HeroClassesAndSkillsLists.java
@@ -0,0 +1,34 @@
+package pl.arturzgodka.controllers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HeroClassesAndSkillsLists {
+
+    public static final List<String> heroClasses = Arrays.asList("Barbarian", "Crusader", "Demon Hunter", "Monk", "Necromancer", "Witch Doctor", "Wizard");
+    public static final List<String> barbarianSkills = Arrays.asList("bash", "hammer-of-the-ancients", "cleave", "ground-stomp", "rend", "leap", "overpower", "frenzy",
+            "seismic-slam", "revenge", "threatening-shout", "sprint", "weapon-throw", "earthquake", "whirlwind", "furious-charge",
+            "ignore-pain", "battle-rage", "call-of-the-ancients", "ancient-spear", "war-cry", "wrath-of-the-berserker", "avalanche");
+    public static final List<String> crusaderSkills = Arrays.asList("punish", "shield-bash", "slash", "shield-glare", "sweep-attack", "iron-skin",
+            "provoke", "smite", "blessed-hammer", "steed-charge", "laws-of-valor", "justice", "consecration", "laws-of-justice", "falling-sword",
+            "blessed-shield", "condemn", "judgment", "laws-of-hope", "akarats-champion", "fist-of-the-heavens", "phalanx", "heavens-fury", "bombardment");
+    public static final List<String> demonHunterSkills = Arrays.asList("hungering-arrow", "impale", "entangling-shot", "caltrops", "rapid-fire", "smoke-screen",
+            "vault", "bolas", "chakram", "preparation", "fan-of-knives", "evasive-fire", "grenade", "shadow-power", "spike-trap", "companion", "strafe",
+            "elemental-arrow", "marked-for-death", "multishot", "sentry", "cluster-arrow", "rain-of-vengeance", "vengeance");
+    public static final List<String> monkSkills = Arrays.asList("fists-of-thunder", "lashing-tail-kick", "deadly-reach", "blinding-flash", "tempest-rush",
+            "breath-of-heaven", "dashing-strike", "crippling-wave", "wave-of-light", "exploding-palm", "cyclone-strike", "way-of-the-hundred-fists",
+            "serenity", "sevensided-strike", "mantra-of-salvation", "sweeping-wind", "mantra-of-retribution", "inner-sanctuary", "mystic-ally",
+            "mantra-of-healing", "mantra-of-conviction", "epiphany");
+    public static final List<String> necromancerSkills = Arrays.asList("bone-spikes", "bone-spear", "grim-scythe", "corpse-explosion", "skeletal-mage", "corpse-lance",
+            "command-skeletons", "siphon-blood", "death-nova", "command-golem", "decrepify", "devour", "leech", "bone-armor", "army-of-the-dead", "frailty",
+            "revive", "bone-spirit", "blood-rush", "land-of-the-dead", "simulacrum");
+    public static final List<String> witchDoctorSkills = Arrays.asList("poison-dart", "grasp-of-the-dead", "corpse-spiders", "summon-zombie-dogs", "firebats", "horrify",
+            "soul-harvest", "plague-of-toads", "haunt", "sacrifice", "zombie-charger", "spirit-walk", "spirit-barrage", "gargantuan", "locust-swarm", "firebomb",
+            "hex", "acid-cloud", "mass-confusion", "big-bad-voodoo", "wall-of-death", "fetish-army", "piranhas");
+    public static final List<String> wizardSkills = Arrays.asList("magic-missile", "ray-of-frost", "shock-pulse", "frost-nova", "arcane-orb", "diamond-skin",
+            "wave-of-force", "spectral-blade", "arcane-torrent", "energy-twister", "ice-armor", "electrocute", "slow-time", "storm-armor", "explosive-blast",
+            "magic-weapon", "hydra", "disintegrate", "familiar", "teleport", "mirror-image", "meteor", "blizzard", "energy-armor", "archon", "black-hole");
+
+}
--- src/main/java/pl/arturzgodka/controllers/SkillsController.java
+++ src/main/java/pl/arturzgodka/controllers/SkillsController.java
@@ -4,8 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 import pl.arturzgodka.datamodel.HeroSkillDataModel;
 import pl.arturzgodka.jsonmappers.SkillMapper;
 
@@ -13,46 +11,20 @@ import java.util.*;
 
 @Controller
 public class SkillsController {
-
-    private final List<String> heroClasses = Arrays.asList("Barbarian", "Crusader", "Demon Hunter", "Monk", "Necromancer", "Witch Doctor", "Wizard");
-    private final List<String> barbarianSkills = Arrays.asList("bash", "hammer-of-the-ancients", "cleave", "ground-stomp", "rend", "leap", "overpower", "frenzy",
-            "seismic-slam", "revenge", "threatening-shout", "sprint", "weapon-throw", "earthquake", "whirlwind", "furious-charge",
-            "ignore-pain", "battle-rage", "call-of-the-ancients", "ancient-spear", "war-cry", "wrath-of-the-berserker", "avalanche");
-    private final List<String> crusaderSkills = Arrays.asList("punish", "shield-bash", "slash", "shield-glare", "sweep-attack", "iron-skin",
-            "provoke", "smite", "blessed-hammer", "steed-charge", "laws-of-valor", "justice", "consecration", "laws-of-justice", "falling-sword",
-            "blessed-shield", "condemn", "judgment", "laws-of-hope", "akarats-champion", "fist-of-the-heavens", "phalanx", "heavens-fury", "bombardment");
-    private final List<String> demonHunterSkills = Arrays.asList("hungering-arrow", "impale", "entangling-shot", "caltrops", "rapid-fire", "smoke-screen",
-            "vault", "bolas", "chakram", "preparation", "fan-of-knives", "evasive-fire", "grenade", "shadow-power", "spike-trap", "companion", "strafe",
-            "elemental-arrow", "marked-for-death", "multishot", "sentry", "cluster-arrow", "rain-of-vengeance", "vengeance");
-    private final List<String> monkSkills = Arrays.asList("fists-of-thunder", "lashing-tail-kick", "deadly-reach", "blinding-flash", "tempest-rush",
-            "breath-of-heaven", "dashing-strike", "crippling-wave", "wave-of-light", "exploding-palm", "cyclone-strike", "way-of-the-hundred-fists",
-            "serenity", "sevensided-strike", "mantra-of-salvation", "sweeping-wind", "mantra-of-retribution", "inner-sanctuary", "mystic-ally",
-            "mantra-of-healing", "mantra-of-conviction", "epiphany");
-    private final List<String> necromancerSkills = Arrays.asList("bone-spikes", "bone-spear", "grim-scythe", "corpse-explosion", "skeletal-mage", "corpse-lance",
-            "command-skeletons", "siphon-blood", "death-nova", "command-golem", "decrepify", "devour", "leech", "bone-armor", "army-of-the-dead", "frailty",
-            "revive", "bone-spirit", "blood-rush", "land-of-the-dead", "simulacrum");
-    private final List<String> witchDoctorSkills = Arrays.asList("poison-dart", "grasp-of-the-dead", "corpse-spiders", "summon-zombie-dogs", "firebats", "horrify",
-            "soul-harvest", "plague-of-toads", "haunt", "sacrifice", "zombie-charger", "spirit-walk", "spirit-barrage", "gargantuan", "locust-swarm", "firebomb",
-            "hex", "acid-cloud", "mass-confusion", "big-bad-voodoo", "wall-of-death", "fetish-army", "piranhas");
-    private final List<String> wizardSkills = Arrays.asList("magic-missile", "ray-of-frost", "shock-pulse", "frost-nova", "arcane-orb", "diamond-skin",
-            "wave-of-force", "spectral-blade", "arcane-torrent", "energy-twister", "ice-armor", "electrocute", "slow-time", "storm-armor", "explosive-blast",
-            "magic-weapon", "hydra", "disintegrate", "familiar", "teleport", "mirror-image", "meteor", "blizzard", "energy-armor", "archon", "black-hole");
-
-    private final Map<String, List<String>> heroClassSkillsMap = new HashMap<String, List<String>>() {{
-        put("barbarian", barbarianSkills);
-        put("crusader", crusaderSkills);
-        put("demon-hunter", demonHunterSkills);
-        put("monk", monkSkills);
-        put("necromancer", necromancerSkills);
-        put("witch-doctor", witchDoctorSkills);
-        put("wizard", wizardSkills);
-    }};
-
     private final SkillMapper skillMapper = new SkillMapper();
+    private final Map<String, List<String>> heroClassSkillsMap = new HashMap<String, List<String>>() {{
+        put("barbarian", HeroClassesAndSkillsLists.barbarianSkills);
+        put("crusader", HeroClassesAndSkillsLists.crusaderSkills);
+        put("demon-hunter", HeroClassesAndSkillsLists.demonHunterSkills);
+        put("monk", HeroClassesAndSkillsLists.monkSkills);
+        put("necromancer", HeroClassesAndSkillsLists.necromancerSkills);
+        put("witch-doctor", HeroClassesAndSkillsLists.witchDoctorSkills);
+        put("wizard", HeroClassesAndSkillsLists.wizardSkills);
+    }};
 
     @RequestMapping("/heroClasses.html")
     public String getHeroesList(Model model) { //model przakzuje aby miec pelne MVC
-        model.addAttribute("heroClassesList", heroClasses); //model.addStribute podake nazwe zmiennje i skad ma pchodzic wartosc dla tej zmiennej.
+        model.addAttribute("heroClassesList", HeroClassesAndSkillsLists.heroClasses); //model.addStribute podake nazwe zmiennje i skad ma pchodzic wartosc dla tej zmiennej.
         return "heroClasses";
     }
 
@@ -60,12 +32,11 @@ public class SkillsController {
     public String getSkills(Model model, @PathVariable(value="heroClasses") String heroClass) {
 
         String heroClassLoverCaseWithHyphonSeparator = heroClass.toLowerCase().replace(" ", "-");
-        String heroClassLoverCaseWithUnderscoreSeparator = heroClass.toLowerCase().replace(" ", "_");
-        List<String> heroClassSkills = heroClassSkillsMap.get(heroClassLoverCaseWithHyphonSeparator);
-        List<HeroSkillDataModel> skillsMapped = skillMapper.fetchSkills(heroClassLoverCaseWithHyphonSeparator, heroClassSkills);
+        List<HeroSkillDataModel> skillsMapped = skillMapper.fetchSkills(heroClassLoverCaseWithHyphonSeparator, heroClassSkillsMap.get(heroClassLoverCaseWithHyphonSeparator)); //z innej klasy nie moge tak zrobic bo dostaję błąd
 
-        model.addAttribute(heroClassLoverCaseWithUnderscoreSeparator + "Skills", skillsMapped);
-        return heroClassLoverCaseWithHyphonSeparator + "Skills";
+        model.addAttribute("skills", skillsMapped);
+        model.addAttribute("heroClass", heroClass);
+        return "skills";
     }
 
 }
