7a6245330c04bbb71ad4b913448d11d45ddb85c4
Arturnix <artur.zgodka.programista@gmail.com>
2024-06-28 23:36:14 +0200

    Added lists of helemets

---
+++ src/main/java/pl/arturzgodka/controllers/ItemClassesAndNamesLists.java
@@ -0,0 +1,34 @@
+package pl.arturzgodka.controllers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ItemClassesAndNamesLists {
+
+    private static final List<String> helmets = Arrays.asList("veil-of-steel-p43_RetroHelm_003", "leorics-crown-Unique_Helm_002_p1", "harlequin-crest-p43_RetroHelm_001", "the-undead-crown-p43_RetroHelm_002",
+            "mystery-helm-PH_Helm", "helm-of-the-cranial-crustacean-TransmogHelm_002", "star-helm-TransmogHelm_001", "leather-hood-Helm_002", "prides-fall-Unique_Helm_103_x1", "broken-crown-P2_Unique_Helm_001",
+            "blind-faith-Unique_Helm_007_x1", "deathseers-cowl-Unique_Helm_102_x1", "warhelm-of-kassar-P4_Unique_Helm_102", "visage-of-gunes-P4_Unique_Helm_103", "basinet-Helm_102", "mask-of-scarlet-death-P6_Necro_Unique_Helm_21",
+            "skull-of-resonance-Unique_Helm_004_x1", "stechhelm-Helm_202", "zischagge-Helm_203", "natalyas-sight-Unique_Helm_009_x1", "mempo-of-twilight-P74_Unique_Helm_006", "tal-rashas-guise-of-wisdom-Unique_Helm_010_x1",
+            "immortal-kings-triumph-Unique_Helm_008_x1", "andariels-visage-Unique_Helm_003_p2", "hellscape-mask-Helm_204", "hellscape-mask-Helm_204", "archon-crown-Helm_206", "fates-vow-P61_Necro_Unique_Helm_22",
+            "jade-harvesters-wisdom-Unique_Helm_Set_09_x1", "sunwukos-crown-Unique_Helm_Set_11_x1", "vyrs-sightless-skull-Unique_Helm_Set_13_x1", "crown-of-the-invoker-Unique_Helm_Set_12_x1", "the-shadows-mask-Unique_Helm_Set_14_x",
+            "eyes-of-the-earth-Unique_Helm_Set_15_x1", "raekors-will-Unique_Helm_Set_05_x1", "helm-of-the-wastes-Unique_Helm_Set_01_p2", "skull-of-savages-P68_Unique_Helm_Set_05", "crown-of-the-light-Unique_Helm_Set_03_p3",
+            "crown-of-valor-P67_Unique_Helm_Set_01", "rolands-visage-Unique_Helm_Set_01_p1", "helm-of-akkhan-Unique_Helm_Set_10_x1", "accursed-visage-Unique_Helm_Set_03_p2", "dystopian-goggles-P69_Unique_Helm_Set_06",
+            "marauders-visage-Unique_Helm_Set_07_x1", "mask-of-the-searing-sky-Unique_Helm_Set_08_x1", "ulianas-spirit-Unique_Helm_Set_01_p3", "decree-of-justice-P67_Unique_Helm_Set_02", "firebirds-plume-Unique_Helm_Set_06_x1",
+            "shrouded-mask-Unique_Helm_Set_02_p2", "typhons-frons-P68_Unique_Helm_Set_03", "arachyrs-visage-Unique_Helm_Set_02_p3", "helltooth-mask-Unique_Helm_Set_16_x1", "luxurious-bauta-P69_Necro_Set_5_Helm",
+            "tragouls-guise-P6_Necro_Set_2_Helm", "inariuss-understanding-P6_Necro_Set_3_Helm", "pestilence-mask-P6_Necro_Set_4_Helm", "rathmas-skull-helm-P6_Necro_Set_1_Helm");
+
+    private static final List<String> spiritStones = Arrays.asList("glass-star-SpiritStone_001", "thought-mirror-SpiritStone_002", "see-no-evil-Unique_SpiritStone_005_x1", "third-eye-SpiritStone_003", "gyana-na-kashu-Unique_SpiritStone_004_x1",
+            "onyx-cluster-SpiritStone_004", "the-minds-eye-Unique_SpiritStone_002_x1", "meditation-crystal-SpiritStone_004a", "erlang-shen-Unique_SpiritStone_003_x1", "eye-of-peshkov-Unique_SpiritStone_103_x1", "kekegis-unbreakable-spirit-Unique_SpiritStone_102_x1",
+            "sunstone-SpiritStone_101", "the-laws-of-seph-Unique_SpiritStone_101_x1", "prism-SpiritStone_102", "seer-shard-SpiritStone_103", "bezoar-stone-Unique_SpiritStone_001_x1", "the-eye-of-the-storm-Unique_SpiritStone_006_x1", "igneous-studs-SpiritStone_104",
+            "solarius-SpiritStone_201", "meditator-SpiritStone_202", "ghost-sight-SpiritStone_203", "innas-radiance-Unique_SpiritStone_009_x1", "madstone-P1_Unique_SpiritStone_008", "star-crown-SpiritStone_204", "tzo-krins-gaze-Unique_SpiritStone_007_x1",
+            "insight-stone-SpiritStone_205");
+
+    private static final List<String> voodooMasks = Arrays.asList("tribal-mask-VoodooMask_001", "split-tusk-Unique_VoodooMask_006_x1", "shaman-mask-VoodooMask_002", "chieftain-mask-VoodooMask_003", "quetzalcoatl-Unique_VoodooMask_005_x1", "totemic-mask-VoodooMask_004a",
+            "carnevil-P65_Unique_VoodooMask_101_x1", "mask-of-jeram-P61_Unique_VoodooMask_102_x1", "woodwraith-bark-VoodooMask_101", "burial-mask-VoodooMask_102", "naga-mask-VoodooMask_103", "umbra-mask-VoodooMask_104", "the-grin-reaper-Unique_VoodooMask_002_x1",
+            "screaming-mask-VoodooMask_201", "underworld-mask-VoodooMask_202", "visage-of-giyua-Unique_VoodooMask_008_x1", "zunimassas-vision-Unique_VoodooMask_007_x1", "tiklandian-visage-Unique_VoodooMask_001_x1", "manitou-mask-VoodooMask_203", "deva-VoodooMask_204",
+            "tribal-mask-VoodooMask_205", "mundunugus-headdress-P68_Unique_Helm_Set_04");
+
+    private static final List<String> wizardHats = Arrays.asList("wanderer-hat-WizardHat_001", "journey-mask-WizardHat_002", "scholarly-fez-WizardHat_003", "crown-of-the-primus-P74_Unique_WizardHat_104", "charlatan-cap-WizardHat_004", "the-swami-P3_Unique_WizardHat_003",
+            "dark-mages-shade-Unique_WizardHat_001_x1", "mage-crown-WizardHat_004a", "archmages-vicalyke-Unique_WizardHat_101_x1", "velvet-camaral-Unique_WizardHat_102_x1", "conjurer-shade-WizardHat_101", "the-magistrate-P68_Unique_WizardHat_103", "mystic-shade-WizardHat_102",
+            "mentor-cap-WizardHat_103", "traveler-hat-WizardHat_104", "sage-hat-WizardHat_201", "sinister-mask-WizardHat_202", "storm-crow-Unique_WizardHat_004_x1", "archmage-headpiece-WizardHat_203", "elder-hat-WizardHat_204", "vizier-hat-WizardHat_205");
+}
