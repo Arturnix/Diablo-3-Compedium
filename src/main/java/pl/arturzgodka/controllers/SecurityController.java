c619c769a16c8e854de3e564257eddd7fbf1b593
Arturnix <artur.zgodka.programista@gmail.com>
2024-09-07 18:51:09 +0200

    Add controller to handle logged user data

--- src/main/java/pl/arturzgodka/controllers/SecurityController.java
+++ src/main/java/pl/arturzgodka/controllers/SecurityController.java
@@ -1,10 +1,13 @@
 package pl.arturzgodka.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.arturzgodka.databaseutils.UserDao;
 import pl.arturzgodka.datamodel.*;
 
@@ -28,13 +31,23 @@ public class SecurityController {
     @PostMapping("/register")
     public String registerUser(UserDataModel user) {
         UserDao dao = new UserDao();
-        List<CharacterDataModel> emptyCharactersListForNewUser = new ArrayList<>();
+        CharactersForAccountProvider charactersForAccountProvider = new CharactersForAccountProvider();
 
-        //user.setCharacters(charactersForAccountProvider.assignUserToCharactersOnProvidedAccount(user));
-        user.setCharacters(emptyCharactersListForNewUser);
+        user.setCharacters(charactersForAccountProvider.assignUserToCharactersOnProvidedAccount(user));
         dao.saveUser(user);
 
-        return "index";
+        return "redirect:" + "index";
+    }
+
+  @GetMapping("/profile")
+    public String getEmailFromLoginForm(Authentication auth, Model model) {
+      UserDao dao = new UserDao();
+      String email = auth.getName();
+      UserDataModel user = dao.findUserByEmail(email);
+
+      model.addAttribute(user);
+
+        return "security/profile";
     }
 
 }
