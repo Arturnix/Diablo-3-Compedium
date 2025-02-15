386c18a4608d984b47c83cede79fa6d7bc656842
Arturnix <artur.zgodka.programista@gmail.com>
2025-02-15 16:27:05 +0100

    Added HeroItemsApi to fetch data with items equipped by hero

---
+++ src/main/java/pl/arturzgodka/apihandlers/HeroItemsApi.java
@@ -0,0 +1,16 @@
+package pl.arturzgodka.apihandlers;
+
+import pl.arturzgodka.token.FetchToken;
+import pl.arturzgodka.token.Token;
+
+public class HeroItemsApi {
+    //Hero ID: 170761702
+    public static String generateRequest(String battleTag, String heroId, FetchToken fetchToken) {
+
+        String apiEndpoint = BaseUrlParts.BASE_PROFILE_API + battleTag.replace('#', '-') +
+                BaseUrlParts.BASE_HERO_API + heroId;
+        String localeAndToken = BaseUrlParts.BASE_LOCALE_AND_TOKEN_FOR_HERO_ITEMS + Token.getAccess_token();
+
+        return fetchToken.fetchAPIResourceRequest(apiEndpoint + localeAndToken);
+    }
+}
